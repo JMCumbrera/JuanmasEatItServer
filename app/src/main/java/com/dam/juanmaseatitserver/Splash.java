0ef4db671cead9c7e5690e4d7d74d8aa64eceeb5
Juan Manuel <jcumlop667@g.educaand.es>
2023-05-28 20:57:50 +0200

    - Arreglado IndexOutOfBounds y Firebase Database paths must not contain '.', '#', '$', '[', or ']'

---
+++ app/src/main/java/com/dam/juanmaseatitserver/Splash.java
@@ -0,0 +1,25 @@
+package com.dam.juanmaseatitserver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int duracion_Splash = 3000;
+
+        // El handler ejecutará el código en el tiempo que indiquemos
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+            };
+        }, duracion_Splash);
+    }
+}
